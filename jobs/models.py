8b6a3267bebe0ef1a87fee6417da8e5cc8ee2039
Paweł Woźniak <100246846+PawelW02@users.noreply.github.com>
2024-10-13 01:34:57 +0200

    Jobs (#3)
    * Job details
    
    * dodanie formularzy dodawania, edycji i usuwania ofert dla zalogowanych użytkowników
    
    * usunięte niepotrzebne elementy

--- jobs/models.py
+++ jobs/models.py
@@ -1,14 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Job(models.Model):
     title = models.CharField(max_length=128)
-    employer = models.CharField(max_length=128)
+    employer = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
     location = models.CharField(max_length=128)
     typeOfWork = models.CharField(max_length=128, help_text="full-time, part-time, internship or other")
     contract = models.CharField(max_length=128, help_text="work contract, civil contract or B2B")
     operatingMode = models.CharField(max_length=128, help_text="remote, stationary or hybrid")
     jobDescription = models.TextField()
     requiredSkills = models.TextField()
-    dateAdded = models.DateField()
+    dateAdded = models.DateField(auto_now_add=True)
     dateExpired = models.DateField()
     salary = models.IntegerField()
