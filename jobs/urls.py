8bf9aadf2f19a45f64c97856b7db465179f90ce9
Paweł <paawlicho1@gmail.com>
2024-10-12 18:15:54 +0200

    Job details

--- jobs/urls.py
+++ jobs/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from jobs import views
 
 urlpatterns = [
-    path('', views.jobOffers, name='offers')
+    path('', views.jobOffers, name='offers'),
+    path('<id>/', views.jobDetails, name='details')
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -4,3 +4,7 @@ from jobs.models import Job
 def jobOffers(request):
     jobs = Job.objects.all()
     return render(request, "jobs/jobs_list.html", context={'jobs': jobs})
+
+def jobDetails(request, id):
+    details = Job.objects.get(pk=id)
+    return render(request, "jobs/job_details.html", context={'offer': details})
