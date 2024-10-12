5780650d09967c6d4eae99dd52245d786b1f213f
DorianSzlachcic <dorianszlachcic@gmail.com>
2024-10-12 13:01:49 +0200

    auth to accounts

--- BHC/settings.py
+++ BHC/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'auth',
+    'accounts',
     'jobs',
 ]
 
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'BHC.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
